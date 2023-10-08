9b1c1063170d6582403736f67734f79c49432be7
RIMaker <zhoraagadzhanyan@gmail.com>
2023-10-08 15:50:26 +0300

    Initial Commit

---
+++ DHCollectionView/DHCollectionView.h
@@ -0,0 +1,18 @@
+//
+//  DHCollectionView.h
+//  DHCollectionView
+//
+//  Created by Zhora Agadzhanyan on 08.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DHCollectionView.
+FOUNDATION_EXPORT double DHCollectionViewVersionNumber;
+
+//! Project version string for DHCollectionView.
+FOUNDATION_EXPORT const unsigned char DHCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DHCollectionView/PublicHeader.h>
+
+
